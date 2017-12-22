8f5d387db905c3aac186ee9bbe0eeb6d1873f71d
Merve Yapnaz <merveyapnaz@hotmail.com>
2017-12-23 01:09:12 +0300

    Arayüz

---
+++ DSTS/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using DSTS.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DSTS.Controllers
+{
+	public class HomeController : Controller
+	{
+		DemirbasTakipEntities db = new DemirbasTakipEntities();
+
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		[AllowAnonymous]
+		public ActionResult Login()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Login(string kulAdi, string parola)
+		{
+			if(db.tblPersonels.Any(x=> x.kullaniciAdi==kulAdi && x.personelParola==parola))
+			{
+				return RedirectToAction("Index", "Home");
+			}
+			else
+			{
+				ViewBag.mesaj = "Kullanıcı adı veya parola hatalı !";
+				return View();
+			}
+		}
+	}
+}
---
+++ DSTS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DSTS.MvcApplication" Language="C#" %>
