bf21ea8f38c04f82141dd3b3e16da9702d71fc1e
ozgealgan <ozgealgn1@gmail.com>
2017-12-23 21:22:42 +0300

    Layoutta düzenleme yapıldı.

--- DSTS/Controllers/KayitController.cs
+++ DSTS/Controllers/KayitController.cs
@@ -16,13 +16,13 @@ namespace DSTS.Controllers
             return View();
         }
 
-        [HttpPost]
+      /*  [HttpPost]
         public ActionResult StokEkle(string fakulteId, string dbAdi, string DbAdet, string dbFiyat, string dbtur, string dbMarka, string dbModel)
         {
            
             bl.StokEkle(Convert.ToInt32(fakulteId), dbAdi, Convert.ToInt32(DbAdet), Convert.ToDecimal(dbFiyat), Convert.ToInt32(dbtur), dbMarka, dbModel);
             return View();
-        }
+        }*/
 
         public ActionResult OdaEkle()
         {
