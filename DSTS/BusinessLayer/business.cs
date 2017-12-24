9027bb41c44973acfbca05dbfa8bbbc473fb0081
ozgealgan <ozgealgn1@gmail.com>
2017-12-24 03:40:15 +0300

    Demirbaş sayısını arama yapıldı

--- DSTS/BusinessLayer/business.cs
+++ DSTS/BusinessLayer/business.cs
@@ -131,6 +131,143 @@ namespace DSTS.BusinessLayer
 			}
 			return ld;
 		}
+
+		public List<localDemirbas> spDemirbasAdinaGoreAra(string demirbasAdi)
+		{
+			List<localDemirbas> ld = new List<localDemirbas>();
+			using (SqlConnection conn = new SqlConnection(conStrig))
+			{
+				conn.Open();
+				SqlCommand cmdd = new SqlCommand("spDemirbasAdinaGoreAra", conn);
+				cmdd.Parameters.Add(new SqlParameter("@demirbasAdi", demirbasAdi));
+
+				cmdd.CommandTimeout = 600;
+				cmdd.CommandType = CommandType.StoredProcedure;
+				SqlDataReader rd = cmdd.ExecuteReader();
+				if (rd.HasRows == true)
+				{
+					while (rd.Read())
+					{
+						localDemirbas d = new localDemirbas();
+						d.demirbasKod = rd["demirbasKodu"].ToString();
+						d.demirbasAdi = rd["demirbasAdi"].ToString();
+						d.demirbasAdet = (int) rd["demirbasAdet"];
+						d.tur = rd["turAdi"].ToString();
+						d.demirbasFiyat = (decimal) rd["fiyat"];
+						d.demirbasTarih = Convert.ToDateTime(rd["alimTarihi"]);
+						d.odaAdi=rd["odaAdi"].ToString();
+						d.adet = (int)rd["adet"];
+						ld.Add(d);
+					}
+				}
+				conn.Dispose();
+				conn.Close();
+			}
+			return ld;
+		}
+
+		public List<localDemirbas> spTuruneGoreAra(string turAdi)
+		{
+			List<localDemirbas> ld = new List<localDemirbas>();
+			using (SqlConnection conn = new SqlConnection(conStrig))
+			{
+				conn.Open();
+				SqlCommand cmdd = new SqlCommand("spTuruneGoreAra", conn);
+				cmdd.Parameters.Add(new SqlParameter("@turAdi", turAdi));
+
+				cmdd.CommandTimeout = 600;
+				cmdd.CommandType = CommandType.StoredProcedure;
+				SqlDataReader rd = cmdd.ExecuteReader();
+				if (rd.HasRows == true)
+				{
+					while (rd.Read())
+					{
+						localDemirbas d = new localDemirbas();
+						d.demirbasKod = rd["demirbasKodu"].ToString();
+						d.demirbasAdi = rd["demirbasAdi"].ToString();
+						d.demirbasAdet = (int)rd["demirbasAdet"];
+						d.tur = rd["turAdi"].ToString();
+						d.demirbasFiyat = (decimal)rd["fiyat"];
+						d.demirbasTarih = Convert.ToDateTime(rd["alimTarihi"]);
+						d.odaAdi = rd["odaAdi"].ToString();
+						d.adet = (int)rd["adet"];
+						ld.Add(d);
+					}
+				}
+				conn.Dispose();
+				conn.Close();
+			}
+			return ld;
+		}
+
+		public List<localDemirbas> spFiyatınaGoreAra(string fiyat)
+		{
+			List<localDemirbas> ld = new List<localDemirbas>();
+			using (SqlConnection conn = new SqlConnection(conStrig))
+			{
+				conn.Open();
+				SqlCommand cmdd = new SqlCommand("spFiyatınaGoreAra", conn);
+				cmdd.Parameters.Add(new SqlParameter("@fiyat", fiyat));
+
+				cmdd.CommandTimeout = 600;
+				cmdd.CommandType = CommandType.StoredProcedure;
+				SqlDataReader rd = cmdd.ExecuteReader();
+				if (rd.HasRows == true)
+				{
+					while (rd.Read())
+					{
+						localDemirbas d = new localDemirbas();
+						d.demirbasKod = rd["demirbasKodu"].ToString();
+						d.demirbasAdi = rd["demirbasAdi"].ToString();
+						d.demirbasAdet = (int)rd["demirbasAdet"];
+						d.tur = rd["turAdi"].ToString();
+						d.demirbasFiyat = (decimal)rd["fiyat"];
+						d.demirbasTarih = Convert.ToDateTime(rd["alimTarihi"]);
+						d.odaAdi = rd["odaAdi"].ToString();
+						d.adet = (int)rd["adet"];
+						ld.Add(d);
+					}
+				}
+				conn.Dispose();
+				conn.Close();
+			}
+			return ld;
+		}
+
+		public List<localDemirbas> spTariheGoreAra(string tarih)
+		{
+			List<localDemirbas> ld = new List<localDemirbas>();
+			using (SqlConnection conn = new SqlConnection(conStrig))
+			{
+				conn.Open();
+				SqlCommand cmdd = new SqlCommand("spTariheGoreAra", conn);
+				cmdd.Parameters.Add(new SqlParameter("@tarih", tarih));
+
+				cmdd.CommandTimeout = 600;
+				cmdd.CommandType = CommandType.StoredProcedure;
+				SqlDataReader rd = cmdd.ExecuteReader();
+				if (rd.HasRows == true)
+				{
+					while (rd.Read())
+					{
+						localDemirbas d = new localDemirbas();
+						d.demirbasKod = rd["demirbasKodu"].ToString();
+						d.demirbasAdi = rd["demirbasAdi"].ToString();
+						d.demirbasAdet = (int)rd["demirbasAdet"];
+						d.tur = rd["turAdi"].ToString();
+						d.demirbasFiyat = (decimal)rd["fiyat"];
+						d.demirbasTarih = Convert.ToDateTime(rd["alimTarihi"]);
+						d.odaAdi = rd["odaAdi"].ToString();
+						d.adet = (int)rd["adet"];
+						ld.Add(d);
+					}
+				}
+				conn.Dispose();
+				conn.Close();
+			}
+			return ld;
+		}
+
 	}
 
 }
